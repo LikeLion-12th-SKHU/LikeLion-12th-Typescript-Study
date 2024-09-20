e82b5d73f43e8afe6e32fb6dcb13fc82155c9bff
yeeun park <145003970+yengniws@users.noreply.github.com>
2024-09-20 17:45:44 +0900

    Feat: week2

---
+++ yeeun/week2/src/chapter4.ts
@@ -0,0 +1,52 @@
+// 타입 별칭
+type User = {
+  id: number;
+  name: string;
+  nickname: string;
+  birth: string;
+  bio: string;
+  location: string;
+};
+
+//   function test() {
+//     type User = string;
+//   }
+
+let user = {
+  id: 1,
+  name: "이정환",
+  nickname: "winterlood",
+  birth: "1997.01.07",
+  bio: "안녕하세요",
+  location: "부천시",
+};
+let user2 = {
+  id: 2,
+  name: "홍길동",
+  nickname: "winterlood",
+  birth: "1997.01.07",
+  bio: "안녕하세요",
+  location: "부천시",
+};
+export {};
+
+// 인덱스 시그니처
+
+type CountryCodes = {
+  [key: string]: string;
+};
+
+let countryCodes: CountryCodes = {
+  Korea: "ko",
+  UnitedState: "us",
+  UnitedKingdom: "uk",
+};
+
+type CountryNumberCodes = {
+  [key: string]: number;
+  Korea: number;
+};
+
+//   let countryNumberAndStringCodes: CountryNumberCodes = {
+//     Korea: "ko";,
+//   }
